45bf9ac522ce37b0217a6b4b26b787ed584abdf3
duhongming1990 <13342496610@189.cn>
2018-09-02 18:27:52 +0800

    线程安全性-可见性：volatile、synchronized

---
+++ concurrent-safe/src/main/java/com/dhm/safe/volatile_/VolatileDemo.java
@@ -0,0 +1,35 @@
+package com.dhm.safe.volatile_;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * @Author duhongming
+ * @Email 935720334@qq.com
+ * @Date 2018/9/2 17:55
+ */
+@Slf4j
+public class VolatileDemo {
+
+    //volatile不具有原子性，常用于状态的可见：true or false
+    private static volatile int count = 0;
+
+    public static void add(){
+        count++;
+    }
+
+
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        for (int i = 0; i < 10000; i++) {
+            executorService.submit(()->{
+               add();
+            });
+        }
+
+        executorService.shutdown();
+        System.out.println("count = " + count);
+    }
+}
