00a914bfb25b7cf86894eb03dd70ea70d046a045
duhongming <19919902414@189.cn>
2018-09-13 10:18:04 +0800

    J.U.C组件拓展

---
+++ concurrent-aqs/src/main/java/com/dhm/expand/FutureExample.java
@@ -0,0 +1,35 @@
+package com.dhm.expand;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+/**
+ * @Author duhongming
+ * @Email 19919902414@189.cn
+ * @Date 2018/9/13 10:15
+ */
+@Slf4j
+public class FutureExample {
+
+    static class MyCallable implements Callable<String> {
+
+        @Override
+        public String call() throws Exception {
+            log.info("do something in callable");
+            Thread.sleep(5000);
+            return "Done";
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        Future<String> future = executorService.submit(new MyCallable());
+        log.info("do something in main");
+        Thread.sleep(1000);
+        String result = future.get();
+        log.info("result：{}", result);
+    }
+}
---
+++ concurrent-aqs/src/main/java/com/dhm/expand/FutureTaskExample.java
@@ -0,0 +1,28 @@
+package com.dhm.expand;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.FutureTask;
+/**
+ * @Author duhongming
+ * @Email 19919902414@189.cn
+ * @Date 2018/9/13 10:15
+ */
+@Slf4j
+public class FutureTaskExample {
+
+    public static void main(String[] args) throws Exception {
+        FutureTask<String> futureTask = new FutureTask<>(()->{
+                log.info("do something in callable");
+                Thread.sleep(5000);
+                return "Done";
+        });
+
+        new Thread(futureTask).start();
+        log.info("do something in main");
+        Thread.sleep(1000);
+        String result = futureTask.get();
+        log.info("result：{}", result);
+    }
+}
